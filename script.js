34a148b4beff3433ffdc07ac0704c8a35d269b6a
Tim Rooke <timrooke1991@gmail.com>
2017-04-12 12:37:40 +0100

    created game logic

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped a " + cardOne);
+console.log("User flipped a " + cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!");
+	}
+}
