0812c3683ecd4185888e1a61ab828effb87a3017
Tim Rooke <timrooke1991@gmail.com>
2017-04-13 10:07:32 +0100

    created game logic

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+   {
+       rank: 'queen',
+       suit: 'hearts',
+       cardImage: "images/queen-of-hearts.png"
+   },
+   {
+       rank: 'queen',
+       suit: 'diamonds',
+       cardImage: "images/queen-of-diamonds.png"
+   }, 
+   {
+       rank: 'king',
+       suit: 'hearts',
+       cardImage: "images/king-of-hearts.png"
+   },
+   {
+       rank: 'king',
+       suit: 'diamonds',
+       cardImage: "images/king-of-diamonds.png"
+   }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -10,8 +31,11 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("Suit: " + cards[cardId].suit);
+	console.log("Image: " + cards[cardId].cardImage);
+	
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
