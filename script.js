fe739b8ce2db2b8f8e59674670dcd02c26635a1c
Tim Rooke <timrooke1991@gmail.com>
2017-04-13 11:37:55 +0100

    created game logic

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ var cards = [
        cardImage: "images/king-of-diamonds.png"
    }
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -30,16 +31,29 @@ var checkForMatch = function () {
 		}
 }
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log("Suit: " + cards[cardId].suit);
-	console.log("Image: " + cards[cardId].cardImage);
-	
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+
+	// console.log("User flipped " + cards[cardId].rank);
+	// console.log("Suit: " + cards[cardId].suit);
+	// console.log("Image: " + cards[cardId].cardImage);
+
 	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute("src", cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	 for (var i = 0; i < cards.length; i++) {
+	 	var cardElement = document.createElement('img');
+	 	cardElement.setAttribute("src", "images/back.png");
+	 	cardElement.setAttribute("data-id", i);
+	 	cardElement.addEventListener("click", flipCard);
+	 	document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+createBoard();
